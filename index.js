57148f2d37fceecc3f589b32d24398acda8f3fd9
Juan-Garcia07 <143968934+Juan-Garcia07@users.noreply.github.com>
2023-10-03 22:52:17 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const express = require('express')
-
 const app = express()
 
-// http://localhost:3000/login?user=juan&password=123
+app.use(express.json());
 
 app.get('/login', (req, res) => {
     const {user, password} = req.query
@@ -11,23 +10,33 @@ app.get('/login', (req, res) => {
         return
     }
     if(user == 'juan' && password == '123'){
-        res.json({msg: 'Iniciastes sesion con exito'})
+        res.json({msg: 'you sign in succesfully'})
         return
     }
     res.status(400).json({msg: 'Error en el usuario o contraseña'})
 })
 
-//http://localhost:3000/
+// http://localhost:3000/
 app.get('/:nombre', (req, res) => {
     const params = req.params
     res.json({params})
 })
+// http://localhost:3000/login?user=juan&password=123
 
-app.post('/', (req, res) => {
-    res.json({msg: "Hello GET!"})
+app.post('/login', (req, res) => {
+    const {user, password} = req.body
+    if(!user || !password){
+        res.status(400).json({msg: 'you need to provide <user> and <password> parameter'})
+        return
+    }
+    if(user == 'juan' && password == '123'){
+        res.json({msg: 'you sign in succesfully'})
+        return
+    }
+    res.status(404).json({msg: 'Error en el usuario o contraseña'})
 })
 
-app.get('/', (req, res) => {
+app.put('/', (req, res) => {
     res.json({msg: "Hello PUT!"})
 })
 
@@ -41,4 +50,4 @@ app.delete('/', (req, res) => {
 
 
 
-app.listen(3000, () => (console.log('listening on port 3000')))
+app.listen(3000, () => {console.log('listening on port 3000')})
